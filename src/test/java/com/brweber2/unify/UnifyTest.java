1b7ce032ffa7834acca16f6949196f171ee80b06
Bryan Weber <brweber2@yahoo.com>
2012-03-04 13:33:13 -0500

    adding some additional test coverage for unification code

--- src/test/java/com/brweber2/unify/UnifyTest.java
+++ src/test/java/com/brweber2/unify/UnifyTest.java
@@ -2,8 +2,10 @@ package com.brweber2.unify;
 
 import com.brweber2.term.Term;
 import com.brweber2.term.impl.AComplexTerm;
+import com.brweber2.term.impl.AVariable;
 import com.brweber2.term.impl.AnAtom;
 import com.brweber2.term.impl.AnNumeric;
+import com.brweber2.unify.impl.Bindings;
 import com.brweber2.unify.impl.Unify;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -16,14 +18,84 @@ public class UnifyTest {
     @Test
     public void testAtomUnify()
     {
-        shouldUnify( new AnAtom("foo"), new AnAtom("foo") );
-        shouldNotUnify(new AnAtom("foo"), new AnAtom("bar"));
-        shouldNotUnify(new AnAtom("foo"), new AnNumeric("123.4"));
-        shouldNotUnify(new AnAtom("foo"), new AComplexTerm("quux"));
-        shouldNotUnify(new AnAtom("foo"), new AComplexTerm("quux",new AnAtom("foo")));
+        shouldAtomUnify( new AnAtom("foo"), new AnAtom("foo") );
+        shouldNotAtomUnify(new AnAtom("foo"), new AnAtom("bar"));
+        shouldNotAtomUnify(new AnAtom("foo"), new AnNumeric("123.4"));
+        shouldNotAtomUnify(new AnAtom("foo"), new AComplexTerm("quux"));
+        shouldNotAtomUnify(new AnAtom("foo"), new AComplexTerm("quux",new AnAtom("foo")));
     }
     
-    private void shouldUnify( Term a, Term b )
+    @Test
+    public void testComplexTermUnify()
+    {
+        shouldComplexTermUnify( new AComplexTerm("foo"), new AComplexTerm("foo") );
+        shouldComplexTermUnify( new AComplexTerm("foo", new AnAtom("bar")), new AComplexTerm("foo", new AnAtom("bar")) );
+        shouldComplexTermUnify( new AComplexTerm("foo", new AComplexTerm("quux",new AnAtom("bing"))), new AComplexTerm("foo",new AComplexTerm("quux",new AnAtom("bing"))) );
+
+        shouldComplexTermNotUnify(new AComplexTerm("foo"), new AnAtom("foo"));
+        shouldComplexTermNotUnify(new AComplexTerm("foo"), new AComplexTerm("bar"));
+        shouldComplexTermNotUnify( new AComplexTerm("foo", new AnAtom("bar")), new AComplexTerm("foo") );
+        shouldComplexTermNotUnify( new AComplexTerm("foo", new AComplexTerm("quux",new AnAtom("bing"))), new AComplexTerm("foo",new AComplexTerm("quux",new AnAtom("meh"))) );
+    }
+    
+    @Test
+    public void testVariableAtomUnify()
+    {
+        shouldVariableAtomUnify(new AnAtom("foo"), new AVariable("bar"));
+        shouldVariableAtomUnify(new AVariable("bar"), new AnAtom("foo"));
+        Binding binding = new Bindings();
+        binding.instantiate(new AVariable("bar"),new AnAtom("foo"));
+        shouldVariableAtomUnify(new AVariable("bar"), new AnAtom("foo"), binding);
+    }
+
+    @Test
+    public void testVariableAtomNotUnify()
+    {
+        shouldVariableAtomUnify( new AnAtom("foo"), new AVariable("bar") );
+        shouldVariableAtomUnify( new AVariable("bar"), new AnAtom("foo") );
+        Binding binding = new Bindings();
+        binding.instantiate(new AVariable("bar"),new AnAtom("quux"));
+        shouldVariableAtomNotUnify(new AVariable("bar"), new AnAtom("foo"), binding);
+    }
+
+    private void shouldComplexTermUnify( Term a, Term b )
+    {
+        UnifyResult result = a.unify(b);
+        Assert.assertTrue(result.succeeded());
+        Assert.assertTrue(result.bindings().getVariables().isEmpty() );
+    }
+
+    private void shouldComplexTermNotUnify( Term a, Term b )
+    {
+        UnifyResult result = a.unify(b);
+        Assert.assertFalse(result.succeeded());
+        Assert.assertTrue(result.bindings().getVariables().isEmpty() );
+    }
+    
+    private void shouldVariableAtomUnify( Term a, Term b )
+    {
+        shouldVariableAtomUnify(a, b, new Bindings());
+    }
+    
+    private void shouldVariableAtomUnify( Term a, Term b, Binding binding )
+    {
+        UnifyResult resultOne = a.unify(b,binding);
+        Assert.assertTrue(resultOne.succeeded());
+        Assert.assertEquals(resultOne.bindings().getVariables().size(), 1);
+        Assert.assertTrue(resultOne.bindings().getVariables().contains(new AVariable("bar")));
+        Assert.assertEquals(resultOne.bindings().resolve(new AVariable("bar")), new AnAtom("foo") );
+    }
+
+    private void shouldVariableAtomNotUnify( Term a, Term b, Binding binding )
+    {
+        UnifyResult resultOne = a.unify(b,binding);
+        Assert.assertFalse(resultOne.succeeded());
+        Assert.assertEquals(resultOne.bindings().getVariables().size(), 1);
+        Assert.assertTrue(resultOne.bindings().getVariables().contains(new AVariable("bar")));
+        Assert.assertEquals(resultOne.bindings().resolve(new AVariable("bar")), new AnAtom("quux") );
+    }
+    
+    private void shouldAtomUnify( Term a, Term b )
     {
         UnifyResult resultOne = a.unify(b);
         Assert.assertTrue(resultOne.succeeded());
@@ -43,7 +115,7 @@ public class UnifyTest {
         Assert.assertTrue(resultFour.bindings().getVariables().isEmpty() );
     }
 
-    private void shouldNotUnify( Term a, Term b )
+    private void shouldNotAtomUnify( Term a, Term b )
     {
         UnifyResult resultOne = a.unify(b);
         Assert.assertFalse(resultOne.succeeded());
